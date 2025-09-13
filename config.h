c0c9b42564339bd6cbea210b6d6df649cc8384e4
AzuraAce <oliverjbmub@gmail.com>
2025-09-13 18:54:46 +0200

    run sh command to get wifi essid as in-built command is broken

--- config.h
+++ config.h
@@ -66,7 +66,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function      format          argument */
-    { wifi_essid,    "%s | ",          "wlp1s0"},
+    /* { wifi_essid,    "%s | ",          "wlp1s0"}, */
+    { run_command,   "%s | ",          "iwgetid -r" },
     { battery_state, "%s",             "BAT0" },
     { battery_perc,  "%s%% | ",        "BAT0" },
 	{ datetime,      "%s",             "%a, %d %b %Y %H:%M" },
